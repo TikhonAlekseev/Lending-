3e71c5b3512d7040dcddc759ba9fff0f0f33e21d
Tikhon <alexeev.tix@yandex.ru>
2021-02-11 19:44:18 +0300

    finish

--- js/main.js
+++ js/main.js
@@ -1,27 +1,22 @@
 $(document).ready(function() {
-  // Создаем переменые для кнопки и для меню
   var pull = $("#navigation__toggle");
   var nav = $(".navigation ul");
-
-  // Описываем событие при нажатии на кнопку
   $(pull).on("click", function(e) {
-    // Отменяем стандартное поведение ссылки в браузере
+
     e.preventDefault();
 
-    // Открываем/Скрываем меню
     nav.slideToggle();
-    // добавляем active
+ 
     $(this).toggleClass("navigation__toggle-button-active");
   });
 
-  // При изменении размера окна, в большую сторону, если меню было с крыто, показываем его.
   $(window).resize(function() {
     var w = $(window).width();
     if (w > 720 && nav.is(":hidden")) {
       nav.removeAttr("style");
     }
   });
-  // Эта функция скрывает меню после нажатия на какой либо пунтк и прокрутки
+  
   var w = $(window).width();
   if (w < 992) {
     $("nav.navigation a").on("click", function() {
@@ -29,7 +24,6 @@ $(document).ready(function() {
     });
   }
 
-  // Вызов слайдера вверху
   $("#top__slider").owlCarousel({
     singleItem: true,
     navigation: true,
@@ -38,7 +32,6 @@ $(document).ready(function() {
     slideSpeed: 1200
   });
 
-  //slide2id - плавная прокрутка по ссылкам внутри страницы
   $(
     "nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id"
   ).mPageScroll2id({
